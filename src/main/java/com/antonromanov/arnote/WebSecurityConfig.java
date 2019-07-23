ba916471cc3270a2ff1215b4fc19165fbd72b044
Anton Romanov <antonromanov@list.ru>
2019-07-23 17:45:42 +0300

    Прикрутил аутентификацию уже не с inmemory проверкой, а с проверкой через постгрес

--- src/main/java/com/antonromanov/arnote/WebSecurityConfig.java
+++ src/main/java/com/antonromanov/arnote/WebSecurityConfig.java
@@ -2,12 +2,16 @@ package com.antonromanov.arnote;
 
 import com.antonromanov.arnote.security.JWTAuthenticationFilter;
 import com.antonromanov.arnote.security.JWTLoginFilter;
+import com.antonromanov.arnote.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,12 +20,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.Arrays;
 
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		prePostEnabled = true
+)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
 
 
 	@Override
@@ -34,17 +44,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/login").permitAll() // For Test on Browser
 				// Need authentication.
 				.anyRequest().authenticated()
-				//.and().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-				//
 				.and()
-				//
-				// Add Filter 1 - JWTLoginFilter
-				//
 				.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
 						UsernamePasswordAuthenticationFilter.class)
-				//
-				// Add Filter 2 - JWTAuthenticationFilter
-				//
 				.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.cors();
 	}
@@ -66,7 +68,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 
-
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
@@ -76,26 +77,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		String password = "123";
-
-		String encrytedPassword = this.passwordEncoder().encode(password);
-//		System.out.println("Encoded password of 123=" + encrytedPassword);
+	/*	String password = "123";
+		String encryptedPassword = this.passwordEncoder().encode(password);
+		System.out.println("Encoded password of 123=" + encryptedPassword);
 
 		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> mngConfig = auth.inMemoryAuthentication();
 
-		// Defines 2 users, stored in memory.
-		// ** Spring BOOT >= 2.x (Spring Security 5.x)
-		// Spring auto add ROLE_
-		UserDetails u1 = User.withUsername("tom").password(encrytedPassword).roles("USER").build();
-		UserDetails u2 = User.withUsername("jerry").password(encrytedPassword).roles("USER").build();
+		UserDetails u1 = User.withUsername("tom").password(encryptedPassword).roles("USER").build();
+		UserDetails u2 = User.withUsername("jerry").password(encryptedPassword).roles("USER").build();
 
 		mngConfig.withUser(u1);
-		mngConfig.withUser(u2);
+		mngConfig.withUser(u2);*/
 
-		// If Spring BOOT < 2.x (Spring Security 4.x)):
-		// Spring auto add ROLE_
-		// mngConfig.withUser("tom").password("123").roles("USER");
-		// mngConfig.withUser("jerry").password("123").roles("USER");
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
 
 	}
 
