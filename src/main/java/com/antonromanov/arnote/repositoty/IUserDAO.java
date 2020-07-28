cfbbde5b65296389f7a7edb99bc2443413e8e0c0
Latinist <antonromanov@list.ru>
2020-07-28 14:02:07 +0300

    Небольшие изменения с логгерами

--- src/main/java/com/antonromanov/arnote/repositoty/IUserDAO.java
+++ src/main/java/com/antonromanov/arnote/repositoty/IUserDAO.java
@@ -4,6 +4,6 @@ import com.antonromanov.arnote.model.SearchCriteria;
 import com.antonromanov.arnote.model.Wish;
 import java.util.List;
 
-public interface IUserDAO {
+public interface IUserDAO { //todo: на хера это нужно???? может удалим?
 	List<Wish> searchWish(List<SearchCriteria> params);
 }
