5d28471437ec9af8eefdb2738acdb9c111d88f22
Latinist <ABRomanov@sberbank.ru>
2022-06-28 11:26:16 +0300

    Голый сваггер. Без всего

--- copy-front-end.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f ~/IdeaProjects/arNote/src/main/resources/static/*.*
-cp -r ~/IdeaProjects/arNoteUI/dist/arnote/* ~/IdeaProjects/arNote/src/main/resources/static
-
-echo " ========= Done! ============== "
--- dump/wishes_view_not_realized.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW wishes_view_not_realized AS
-  SELECT
-    w.id,
-    w.wish,
-    w.price,
-    w.priority,
-    w.priority_group
-  FROM arnote.public.wishes AS w WHERE w.user_id = 1 AND (w.realized  = FALSE OR w.realized ISNULL) ;
